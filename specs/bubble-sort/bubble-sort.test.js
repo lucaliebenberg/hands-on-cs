1b3ea030d9762509fc42f6c63e61e9f02ce2d497
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-27 22:34:38 +0200

    workin on bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,13 +10,38 @@
   index's. Continue looping through until all values are in ascending order
 */
 
+// function bubbleSort(nums) {
+//   // code goes here
+//   while(nums[i] > nums[i + 1]){
+//     for (i = i + 1; i < nums.length; i++) {
+//       if (nums[i] > nums[i + 1]){
+//         num[i] = nums[i + 1];
+//         num[i + 1] = nums[i];
+//         continue
+//       }
+//     }
+//   }
+// }
+
 function bubbleSort(nums) {
-  // code goes here
+  let swapped = false;
+  do {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums(i + 1)) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while(swapped)
+
+    return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
