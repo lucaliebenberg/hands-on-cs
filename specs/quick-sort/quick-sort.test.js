29ef8d2f86030a61a1bbaa63986735dee024ba7d
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-29 15:57:23 +0200

    workings for quick sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -12,13 +12,63 @@
 
 */
 
+// function quickSort(nums) {
+//   // base case, array of length 1 or 0
+//   if (nums.length < 2) {
+//     return nums;
+//   }
+
+//   // choose pivot
+//   const pivot = nums[nums.length - 1];
+
+//   // separate into left and right arrays
+//   const left = [];
+//   const right = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] < pivot){
+//       left.push(nums[i]);
+//     } else {
+//       right.push(nums[i]);
+//     }
+//     return left, right
+//   }
+
+//   // call quickSort on left and right
+//   const sortedLeft = quickSort(left);
+//   const sortedRight = quickSort(right);
+
+//   // return left.concat(pivot, right)
+//   return sortedLeft.concat(pivot, sortedRight);
+// }
+
 function quickSort(nums) {
-  // code goes here
+  if (nums.length <= 1){ // (length 0 | length 1)
+    return nums;
+  }
+
+  const pivot = nums[nums.length - 1];
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length - 1; i++){ // NOTE: need to -1 from length, so 'pivot' is not included
+    if (nums[i] < pivot){
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i])
+    }
+  }
+
+  const sortedLeft = quickSort(left);
+  const sortedRight = quickSort(right);
+
+  return sortedLeft.concat(pivot, sortedRight);
+  // return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
