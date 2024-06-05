0ba9d903330c7ec7c0129b977f334d1511298cd9
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-05 22:56:23 +0200

    added updates for dfts

--- specs/traversals/depth-first.test.js
+++ specs/traversals/depth-first.test.js
@@ -1,13 +1,28 @@
 const preorderTraverse = (node, array) => {
   // code goes here
+  if (!node) return array;
+  array.push(node.value);
+  array = preorderTraverse(node.left, array);
+  array = preorderTraverse(node.right, array);
+  return array;
 };
 
 const inorderTraverse = (node, array) => {
   // code goes here
+  if (!node) return array;
+  array = inorderTraverse(node.left, array);
+  array.push(node.value);
+  array = inorderTraverse(node.right, array);
+  return array;
 };
 
 const postorderTraverse = (node, array) => {
   // code goes here
+  if (!node) return array;
+  array = postorderTraverse(node.left, array);
+  array = postorderTraverse(node.right, array);
+  array.push(node.value);
+  return array;
 };
 
 // unit tests
