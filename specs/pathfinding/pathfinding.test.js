7fca164fde66019fc5c50c69abd2691c29223bc6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-08 16:17:57 +0200

    added pathfinding workings

--- specs/pathfinding/pathfinding.test.js
+++ specs/pathfinding/pathfinding.test.js
@@ -16,9 +16,64 @@
 // the way I did. however feel free to use it if you'd like
 const logMaze = require("./logger");
 
-function findShortestPathLength(maze, [xA, yA], [xB, yB]) {
-  // code goes here
-}
+const findShortestPathLength = (maze, [xA, yA], [xB, yB]) => {
+  const visited = maze.map((row, y) =>
+    row.map((origin, x) => ({
+      closed: origin === 1,
+      length: 0,
+      openedBy: NO_ONE,
+      x,
+      y
+    }))
+  );
+  visited[yA][xA].openedBy = BY_A;
+  visited[yB][xB].openedBy = BY_B;
+  // logMaze(visited);
+
+  let aQueue = [visited[yA][xA]];
+  let bQueue = [visited[yB][xB]];
+  let iteration = 0;
+
+  // if one runs out, there's no path
+  while (aQueue.length && bQueue.length) {
+    iteration++;
+    const aNeighbors = aQueue.reduce(
+      (acc, neighbor) =>
+        acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)),
+      []
+    );
+    aQueue = [];
+    for (let i = 0; i < aNeighbors.length; i++) {
+      const neighbor = aNeighbors[i];
+      if (neighbor.openedBy === BY_B) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_A;
+        aQueue.push(neighbor);
+      }
+    }
+
+    const bNeighbors = bQueue.reduce(
+      (acc, neighbor) =>
+        acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)),
+      []
+    );
+    bQueue = [];
+    for (let i = 0; i < bNeighbors.length; i++) {
+      const neighbor = bNeighbors[i];
+      if (neighbor.openedBy === BY_A) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_B;
+        bQueue.push(neighbor);
+      }
+    }
+    // logMaze(visited);
+  }
+  return -1;
+};
 
 const getNeighbors = (visited, x, y) => {
   const neighbors = [];
